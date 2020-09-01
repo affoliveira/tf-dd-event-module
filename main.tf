2f27ae73ddc167f3fbd4f6c5be4858f028db705b
Andre Oliveira <affoliveira@gmail.com>
2020-09-01 12:43:16 +0000

    Initial commit

---
+++ main.tf
@@ -0,0 +1,18 @@
+resource "null_resource" "datadog" {
+	provisioner "local-exec" {
+		command = <<EOT
+curl -X POST https://api.datadoghq.com/api/v1/events?api_key=?${var.datadog_api_key} 
+-H "Content-Type: application/json" 
+-d @- << EOF 
+{
+"alert_type": "info",
+"priority": "normal",
+"source_type_name": "terraform",
+"text": "${var.datadog_text}",
+"title": "${var.datadog_title}"
+}
+EOF
+EOT
+	}
+}
+
---
+++ vars.tf
@@ -0,0 +1,11 @@
+variable "datadog_api_key" {
+  type = string
+}
+
+variable "datadog_text" {
+  type = string
+}
+
+variable "datadog_title" {
+  type = string
+}
