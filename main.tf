01eab10db3922380878bae8eefa228855e5c9382
Andre Oliveira <affoliveira@gmail.com>
2020-09-01 13:14:50 +0000

    fixed curl command

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "null_resource" "datadog" {
 	provisioner "local-exec" {
 		command = <<EOT
-curl -X POST https://api.datadoghq.com/api/v1/events?api_key=?${var.datadog_api_key} 
+curl -X POST https://api.datadoghq.com/api/v1/events?api_key=${var.datadog_api_key} 
 -H "Content-Type: application/json" 
 -d @- << EOF 
 {
