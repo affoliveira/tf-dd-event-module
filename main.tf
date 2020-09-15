e542e0621f8a27f9efe3cc460b3d720356c1096a
Andre Oliveira <affoliveira@gmail.com>
2020-09-15 10:10:45 +0000

    added tags and variables for alert and priority

--- main.tf
+++ main.tf
@@ -1,13 +1,15 @@
 resource "null_resource" "datadog" {
+	triggers = {
+		timestamp = timestamp()
+	}
 	provisioner "local-exec" {
 		command = <<EOT
-curl -X POST https://api.datadoghq.com/api/v1/events?api_key=${var.datadog_api_key} 
--H "Content-Type: application/json" 
--d @- << EOF 
+curl -X POST https://api.datadoghq.com/api/v1/events?api_key=${var.datadog_api_key} -H "Content-Type: application/json" -d @- << EOF 
 {
-"alert_type": "info",
-"priority": "normal",
+"alert_type": "${var.datadog_alert_type}",
+"priority": "${var.datadog_priority}",
 "source_type_name": "terraform",
+"tags": "${var.datadog_tags}",
 "text": "${var.datadog_text}",
 "title": "${var.datadog_title}"
 }
@@ -15,4 +17,3 @@ EOF
 EOT
 	}
 }
-
--- vars.tf
+++ vars.tf
@@ -2,6 +2,21 @@ variable "datadog_api_key" {
   type = string
 }
 
+variable "datadog_tags" {
+  type = string
+  default = null
+}
+
+variable "datadog_priority" {
+  type = string
+  default = "normal"
+}
+
+variable "datadog_alert_type" {
+  type = string
+  default = "info"
+}
+
 variable "datadog_text" {
   type = string
 }
